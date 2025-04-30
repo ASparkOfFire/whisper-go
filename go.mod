fbe53a7c1c9835c31a49f5dc0b4aa8d31d71b1eb
asparkoffire <kabir.47.chd@gmail.com>
2025-04-30 20:49:56 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asparkoffire/whisper-go
+
+go 1.23
---
+++ pkg/whisper/consts.go
@@ -0,0 +1,27 @@
+package whisper
+
+import (
+	"errors"
+
+	"github.com/asparkoffire/whisper-go"
+)
+
+///////////////////////////////////////////////////////////////////////////////
+// ERRORS
+
+var (
+	ErrUnableToLoadModel    = errors.New("unable to load model")
+	ErrInternalAppError     = errors.New("internal application error")
+	ErrProcessingFailed     = errors.New("processing failed")
+	ErrUnsupportedLanguage  = errors.New("unsupported language")
+	ErrModelNotMultilingual = errors.New("model is not multilingual")
+)
+
+///////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+
+// SampleRate is the sample rate of the audio data.
+const SampleRate = whisper.SampleRate
+
+// SampleBits is the number of bytes per sample.
+const SampleBits = whisper.SampleBits
